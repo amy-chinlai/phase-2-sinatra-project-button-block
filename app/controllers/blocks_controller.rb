967bd6483dc028237d66179a961160c6c1d34a82
amy-chinlai <amy@actionnetwork.org>
2021-10-05 11:46:39 -0400

    Create edit, show, and new forms for blocks

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -5,9 +5,20 @@ class BlocksController < ApplicationController
         erb :'blocks/index'
     end
 
+    get '/:id/blocks/new' do
+        erb :'blocks/new'
+    end
+
     get '/:id/blocks/:block_id' do
         @block = Block.find_by_id(params[:block_id])
         erb :'blocks/show'
     end
+
+    get '/:id/:blocks/:block_id/edit' do
+        @block = Block.find_by_id(params[:block_id])
+        erb :'blocks/edit'
+    end
+
+    
 
 end
