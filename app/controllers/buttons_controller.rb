8696c0d8a11816ce1e09b0d3890b734f99e040e2
amy-chinlai <amy@actionnetwork.org>
2021-10-06 10:58:16 -0400

    Added authentication to button controller

--- app/controllers/buttons_controller.rb
+++ app/controllers/buttons_controller.rb
@@ -1,38 +1,61 @@
 class ButtonsController < ApplicationController
 
     get '/:id/blocks/:block_id/buttons' do
-        @block = Block.find_by_id(params[:block_id])
-        erb :'buttons/index'
+        if authenticated? 
+            @block = Block.find_by_id(params[:block_id])
+            erb :'buttons/index'
+        else
+            redirect to "/login"
+        end
     end
 
     get '/:id/blocks/:block_id/buttons/new' do
-        @block = Block.find_by_id(params[:block_id])
-        erb :'buttons/new'
+        if authenticated? 
+            @block = Block.find_by_id(params[:block_id])
+            erb :'buttons/new'
+        else
+            redirect to "/login"
+        end
     end
 
     get '/:id/blocks/:block_id/buttons/:button_id/edit' do
-        @button = Button.find_by_id(params[:button_id])
-        erb :'buttons/edit'
+        if authenticated? 
+            @button = Button.find_by_id(params[:button_id])
+            erb :'buttons/edit'
+        else
+            redirect to "/login"
+        end
     end
 
     post '/:id/blocks/:block_id/buttons' do
-        @button = Button.new(params[:button])
-        @button.block = Block.find_by_id(params[:block_id])
-        @button.save
-        redirect to "/#{@button.block.user_id}/blocks/#{@button.block.id}"
+        if authenticated? 
+            @button = Button.new(params[:button])
+            @button.block = Block.find_by_id(params[:block_id])
+            @button.save
+            redirect to "/#{@button.block.user_id}/blocks/#{@button.block.id}"
+        else
+            redirect to "/login"
     end
 
     patch '/:id/blocks/:block_id/buttons/:button_id' do
-        @button = Button.find_by_id(params[:button_id])
-        @button.update(params[:button])
-        redirect to "/#{@button.block.user_id}/blocks/#{@button.block.id}"
+        if authenticated? 
+            @button = Button.find_by_id(params[:button_id])
+            @button.update(params[:button])
+            redirect to "/#{@button.block.user_id}/blocks/#{@button.block.id}"
+        else
+            redirect to "/login"
+        end
     end    
 
     delete '/:id/blocks/:block_id/buttons/:button_id/delete' do
-        @button = Button.find_by_id(params[:button_id])
-        @button.delete
-        redirect to "/#{@button.block.user_id}/blocks/#{@button.block.id}"
-        # flash to acknowledge delete
+        if authenticated? 
+            @button = Button.find_by_id(params[:button_id])
+            @button.delete
+            redirect to "/#{@button.block.user_id}/blocks/#{@button.block.id}"
+            # flash to acknowledge delete
+        else
+            redirect to "/login"
+        end
     end
 
 
