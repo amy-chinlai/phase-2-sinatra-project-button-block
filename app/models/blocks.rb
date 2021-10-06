e39eb34a8753f11ba11e40f16db42c9907b31e54
amy-chinlai <amy@actionnetwork.org>
2021-10-06 14:25:15 -0400

    Refactored liquid output

--- app/models/blocks.rb
+++ app/models/blocks.rb
@@ -5,15 +5,6 @@ class Block < ActiveRecord::Base
 
     validates :name, :presence => true
     validates :donation_type, :presence => true
-
-    def liquid_output
-        puts buttons
-        buttons.map do |button|
-            # what I want the output to look like
-            # basic: {{ HighestPreviousContribution | times: 1 | default: 5 }}
-            "{{ #{donation_type} | times: #{button.multiplier} | default: #{button.default} }}<br>"
-        end
-    end
     
     
     end
